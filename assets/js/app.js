ced0145060e06b21788768f66bfaab0c0156d9c3
Nitesh Bharti <niteshbharti005101@gmail.com>
2020-06-22 19:01:41 +0530

    .

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+VANTA.TOPOLOGY({
+    el: ".container1",
+    mouseControls: true,
+    touchControls: true,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0xff0002,
+    backgroundColor: 0x0
+  })
